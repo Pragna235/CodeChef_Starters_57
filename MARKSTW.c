2a12a5f1712fa284dce146001f751c480bd4b5c3
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2022-09-22 21:20:24 +0530

    Add files via upload
    Starters 57 Challenge!

---
+++ MARKSTW.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x,y;
+	scanf("%d %d",&x, &y);
+	if(x>= (2*y))
+	   printf("YES\n");
+	else
+	   printf("NO\n");
+	return 0;
+}
+
---
+++ SUMNEQ.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n;
+	scanf("%d",&n);
+	printf("%d",n-1);
+	return 0;
+}
+
